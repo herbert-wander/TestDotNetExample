60b10cb1553cf12f3253f72436a03659583f21c1
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2022-01-11 18:07:08 -0300

    Small test

--- AppTestCode/Program.cs
+++ AppTestCode/Program.cs
@@ -6,7 +6,7 @@ namespace AppTestCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dio!!");
         }
     }
 }
